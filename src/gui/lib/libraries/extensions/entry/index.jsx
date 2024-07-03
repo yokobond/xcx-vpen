efd776930366de0d01e4be1422ccf695619e25ce
yokobond <koji.yokokawa@gmail.com>
2024-07-03 13:51:57 +0900

    chore: initial scaffolding

---
+++ src/gui/lib/libraries/extensions/entry/index.jsx
@@ -0,0 +1,49 @@
+/**
+ * This is an extension for Xcratch.
+ */
+
+import iconURL from './entry-icon.png';
+import insetIconURL from './inset-icon.svg';
+import translations from './translations.json';
+
+/**
+ * Formatter to translate the messages in this extension.
+ * This will be replaced which is used in the React component.
+ * @param {object} messageData - data for format-message
+ * @returns {string} - translated message for the current locale
+ */
+let formatMessage = messageData => messageData.defaultMessage;
+
+const entry = {
+    get name () {
+        return formatMessage({
+            id: 'xcxVPen.entry.name',
+            defaultMessage: 'Vector Pen',
+            description: 'name of the extension'
+        });
+    },
+    extensionId: 'xcxVPen',
+    extensionURL: 'https://yokobond.github.io/xcx-vpen/dist/xcxVPen.mjs',
+    collaborator: 'yokobond',
+    iconURL: iconURL,
+    insetIconURL: insetIconURL,
+    get description () {
+        return formatMessage({
+            defaultMessage: 'an extension for Xcratch',
+            description: 'Description for this extension',
+            id: 'xcxVPen.entry.description'
+        });
+    },
+    featured: true,
+    disabled: false,
+    bluetoothRequired: false,
+    internetConnectionRequired: false,
+    helpLink: 'https://yokobond.github.io/xcx-vpen/',
+    setFormatMessage: formatter => {
+        formatMessage = formatter;
+    },
+    translationMap: translations
+};
+
+export {entry}; // loadable-extension needs this line.
+export default entry;
